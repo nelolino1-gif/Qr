b1e1a9e0fdedbb73cdac92622da902a9d5c28483
Nel Olino <nelolino1@gmail.com>
2026-05-20 07:29:56 +0800

    4file

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('pwx-store').then((cache) => cache.addAll(['./'])));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
+
+
